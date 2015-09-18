9211fb26e96172fdd344cb412e7d5992833a1cf4
Morten Henriksen <mh@gi2.dk>
2015-09-18 18:09:49 +0200

    bump 1.5.3

--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
   name: 'dispatch:phoneformat.js',
   summary: 'phoneformat.js packaged for meteor',
-  version: '1.5.2',
+  version: '1.5.3',
   git: 'https://github.com/DispatchMe/phoneformat.js.git'
 });
 
 Package.onUse(function (api) {
   api.use([
     // core
+    'underscore',
     'reactive-var@1.0.5',
     'templating@1.1.1',
 
